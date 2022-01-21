cad678f9c35957adab0dac8612cd92ae8d0810b0
Trung Ta <57113885+Ta-Trung@users.noreply.github.com>
2022-01-21 23:28:28 +0200

    done part of blog

--- single.php
+++ single.php
@@ -13,28 +13,15 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<?php
-		while ( have_posts() ) :
-			the_post();
+		while ( have_posts() ) {
+			the_post();?>
 
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'trungta' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'trungta' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
+		<div class="about-header u-center-text u-margin-bottom-big">
+            <h2 class="heading-subpage"><?php the_title() ?></h2>
+            <p class="paragraph-subpage">TÄN ON VAIHDETTAVA</p>
+        </div>
 
 	</main><!-- #main -->
 
-<?php
-get_sidebar();
+<?php }
 get_footer();
