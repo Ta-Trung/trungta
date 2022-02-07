0102fd9983b56745624ce32045a4c73ae8485ac6
Trung Ta <57113885+Ta-Trung@users.noreply.github.com>
2022-02-07 22:25:08 +0200

    implemented 404 page

--- 404.php
+++ 404.php
@@ -8,52 +8,32 @@
  */
 
 get_header();
+pageBanner(array(
+    'title' => "Oops 404!!",
+    'subtitle'  => "Oletko varmaan syöttänyt osoitetta oikein?",
+    'photo' =>  Null
+))
 ?>
 
 	<main id="primary" class="site-main">
-
 		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'trungta' ); ?></h1>
-			</header><!-- .page-header -->
+			<div class="row">
+				<div class="col">
+					<header class="not-found__page-header heading-tertiary">
+						<h1 class="not-found__page-title"><?php esc_html_e( 'Etsimäsi sivu on poistettu tai siiretty muihin osoitteihin', 'trungta' ); ?></h1>
+					</header><!-- .page-header -->
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'trungta' ); ?></p>
+					<div class="not-found__page-content paragraph">
+						<p><?php esc_html_e( 'Käytäthän alla olevaa hakukenttää tehdäksesi hakuja', 'trungta' ); ?></p>
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'trungta' ); ?></h2>
-						<ul>
 							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
+							get_search_form();
 							?>
-						</ul>
-					</div><!-- .widget -->
 
-					<?php
-					/* translators: %1$s: smiley */
-					$trungta_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'trungta' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$trungta_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
+					</div><!-- .page-content -->
+				</div>
+			</div>
 		</section><!-- .error-404 -->
-
 	</main><!-- #main -->
 
 <?php
